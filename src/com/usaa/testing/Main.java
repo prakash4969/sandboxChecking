2798004c8cf9abbfe52fbeecef897dd26d220bc7
sprakash4969@gmail.com <prakashgarikipati999@gmail.com>
2021-08-31 00:19:12 -0400

    adding addition to testing via sandbox

---
+++ src/com/usaa/testing/Main.java
@@ -0,0 +1,12 @@
+package com.usaa.testing;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        addition a = new addition();
+
+        System.out.println(a.add());
+        System.out.println(a.sub());
+    }
+}
---
+++ src/com/usaa/testing/addition.java
@@ -0,0 +1,15 @@
+package com.usaa.testing;
+
+public class addition {
+    int a=10;
+    int b=20;
+    int c =a+b;
+    int d = a-b;
+    public int add(){
+       return c;
+    }
+    public int sub(){
+        return d;
+    }
+
+}
